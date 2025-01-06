9a96a6260a6e54d082c69eb3e8c68499593c487e
Rishi Kewalya <113237646+Rishikewalya@users.noreply.github.com>
2025-01-06 09:25:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sum Pair closest to target/sum-pair-closest-to-target.cpp
+++ Difficulty: Easy/Sum Pair closest to target/sum-pair-closest-to-target.cpp
@@ -10,33 +10,22 @@ using namespace std;
 class Solution {
   public:
     vector<int> sumClosest(vector<int>& arr, int target) {
-        sort(arr.begin() , arr.end());
-        int n = arr.size();
-        int mindiff = INT_MAX;
-        vector<int>ans;
-        int i = 0;int j = n-1;
-        while(i<j)
-        {
-            int diff = abs(target - (arr[i] + arr[j]));
-            if(diff<mindiff)
-            {
-                mindiff = diff;
-                if(ans.empty())
-                {ans.push_back(arr[i]);ans.push_back(arr[j]);}
-                ans[0] = arr[i];
-                ans[1] = arr[j];
+        // code here
+        sort(arr.begin(),arr.end());
+        vector<int> res;
+        int n=arr.size(),i=0,j=n-1,mini=INT_MAX;
+        bool found=false;
+        while(i<j){
+            int sum=arr[i]+arr[j];
+            if(abs(target-sum)<mini){
+                mini=abs(target-sum);
+                res={arr[i],arr[j]};
+                found=true;
             }
-            
-            if(arr[i]+ arr[j] <= target)
-            {
-                i++;
-            }
-            else
-            {
-                j--;
-            }
+            if(sum>target)j--;
+            else i++;
         }
-       return ans;
+        return found?res:vector<int>();
     }
 };
 
